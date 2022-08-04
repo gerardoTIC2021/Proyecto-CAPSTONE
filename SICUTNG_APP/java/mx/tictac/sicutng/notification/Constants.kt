d85de5c37366529ae0c2b15be9cb2127b8b1d2b3
tuxmex <36056832+tuxmex@users.noreply.github.com>
2022-08-03 23:14:01 -0500

    Update Constants.kt

--- SICUTNG_APP/java/mx/tictac/sicutng/notification/Constants.kt
+++ SICUTNG_APP/java/mx/tictac/sicutng/notification/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
         const val SERVER_KEY =
-            "AAAAvQ21WsM:APA91bFFGNmzLTi4AwKB8hEl1687hu8E3J9o_zw1uMt6CrkDvq3HaJJOsjHR0nzilmIN8n_mapfc3t4WvzhxE_idQRUJxSQXnhtgqUnHIugDqUvTirzugxTtzJEogInQb5582Sda2mts"
+            "TYPE_YOUR_SERVER_KEY"
         const val CONTENT_TYPE = "application/json"
     }
 }
