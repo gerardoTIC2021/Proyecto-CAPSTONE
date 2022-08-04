cdeb7ec7f66fd36573cf46d43cbb9350a8afd2c3
tuxmex <36056832+tuxmex@users.noreply.github.com>
2022-08-04 12:47:41 -0500

    Update Constants.kt

--- SICUTNG_APP/java/mx/tictac/sicutng/notification/Constants.kt
+++ SICUTNG_APP/java/mx/tictac/sicutng/notification/Constants.kt
@@ -1,10 +1,21 @@
 package mx.tictac.sicutng.notification
+/*
+ * Constantes para notificaciones Fibebase 
+ * por: El Profe Tacho
+ * Fecha: 10 de julio de 2022
+ * 
+ * Representación de datos estáticos de las clase para poder acceder a la base de datos de firebase
+ *
+ */
 
 class Constants {
     companion object {
+        //Url para las notificaciones.
         const val BASE_URL = "https://fcm.googleapis.com"
+        //Cambiar por tu server key de la aplicacion generada desde consola de firebase.
         const val SERVER_KEY =
             "TYPE_YOUR_SERVER_KEY"
+        //Tipos de contenidos de las notificaciones en este caso JSON
         const val CONTENT_TYPE = "application/json"
     }
 }
