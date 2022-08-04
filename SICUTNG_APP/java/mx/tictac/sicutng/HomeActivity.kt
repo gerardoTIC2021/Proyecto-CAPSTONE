d1ff314dda10fc502e6c13a01ed5e87db61e3ecd
tuxmex <36056832+tuxmex@users.noreply.github.com>
2022-08-04 00:07:13 -0500

    Update HomeActivity.kt

--- SICUTNG_APP/java/mx/tictac/sicutng/HomeActivity.kt
+++ SICUTNG_APP/java/mx/tictac/sicutng/HomeActivity.kt
@@ -1,5 +1,11 @@
     package mx.tictac.sicutng
-
+/*
+ * Pantalla de Autenticación de usuarios
+ * por: El Profe Tacho
+ * Fecha: 10 de julio de 2022
+ * 
+ * Clase de acceso a las distintas funcionalidades del proyecto.
+ */
     import android.content.Context
     import android.content.Intent
     import android.os.Bundle
@@ -36,7 +42,8 @@
             title = "Start"
             emailTextView.text = email
             proveedorTextView.text = provider
-
+            
+            //Botón de Cerrar Sesión
             logOutButton.setOnClickListener {
 
                 // Remove data
@@ -47,27 +54,32 @@
                 FirebaseAuth.getInstance().signOut()
                 onBackPressed()
             }
-
+            
+            //Botón para acceso a la pantalla de solicitud de atención.
             requestAttentionButton.setOnClickListener {
 
                 startActivity(Intent(this, AlertActivity::class.java))
             }
-
+            
+            //Botón de acceso a la pantalla de notificaciones directas al vendedor.
             notificationTestButton.setOnClickListener {
                 startActivity(Intent(this, NotificationActivityTest::class.java))
             }
 
+            //Botón para acceso a la pantalla de sensores de temperatura y humedad. 
             sensorsButton.setOnClickListener {
                 startActivity(Intent(this, SensorsActivity::class.java))
             }
 
+            //Botón para acceso a la pantalla de control de la electricidad de cada una de las areas de la tienda.
             controlButton.setOnClickListener {
                 startActivity(Intent(this, ControlActivity::class.java))
             }
 
         }
     }
-
+    
+    //Tipos de autenticación o de inicio de sesión.
     enum class ProviderType{
         BASIC, GOOGLE
     }
