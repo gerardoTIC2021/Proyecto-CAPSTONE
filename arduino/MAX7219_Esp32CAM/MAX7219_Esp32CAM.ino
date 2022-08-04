000dcb7fb97c59bebbed194dc38d45ff6870e517
gbarron2014 <gbarron@utng.edu.mx>
2022-08-04 15:01:08 -0500

    Update MAX7219_Esp32CAM.ino
    Se documenta de manera mas completa el còdigo

--- arduino/MAX7219_Esp32CAM/MAX7219_Esp32CAM.ino
+++ arduino/MAX7219_Esp32CAM/MAX7219_Esp32CAM.ino
@@ -1,11 +1,11 @@
  /*
  * Conexión MAX7219 con ESP32CAM
- * por: Gabriel Barròn Rodrìguez
+ * por: Anastasio Rodrìguez Garcia, Gabriel Barròn Rodrìguez, Gerardo Reyna Ibarra
  * Fecha: 11 de julio de 2022
  * 
- * Este programa recibe datos a través del protocolo MQTT. Para poder
- * comprobar el funcionamiento de este programa, es necesario conectarse a un broker
- * y usar NodeRed para visualzar que la información se está recibiendo correctamente.
+ * Este programa se conecta a una red Wifi mediante las credenciales de la propia red
+ * El programa recibe datos a través del protocolo MQTT. 
+ * Es necesario usar NodeRed para visualzar la información se está recibiendo correctamente.
  * Este programa no requiere componentes adicionales.
  * 
  * MAX7219     PinESP32CAM
@@ -13,23 +13,26 @@
  * GND------------ GND
  * DIN ------------ 13
  * CS ------------- 15
- * CLK-------------- 14
+ * CLK--------------14
  */
-// Librerias requridas:
-#include "esp_camera.h"
-#include <MD_Parola.h>
-#include <MD_MAX72xx.h>
-#include <SPI.h>
+ 
+// Librerias requeridas:
+#include "esp_camera.h"  //Es necesario para la activacion y detecciòn de la camara
+#include <MD_Parola.h>   //Es necesario para el envìo de mensajes a LEDMATRIX
+#include <MD_MAX72xx.h>  //Necesaria para el actuador de despliegue de mensajes
+#include <SPI.h> 
 #include <WiFi.h>  // Biblioteca para el control de WiFi
 #include <PubSubClient.h> //Biblioteca para conexion MQTT
 
+//Modelo de càmara que se activarà en la ESP32CAM
 #define CAMERA_MODEL_AI_THINKER // Has PSRAM
-#include "camera_pins.h"
+#include "camera_pins.h"  //Archivo que define los pins de la ESP32CAM
 
 // Define tipo hardware, tamaño, y pines de salida:
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-#define MAX_DEVICES 4
-// Setup para software SPI:
+#define MAX_DEVICES 4 //Son cuatro led matrix
+
+// Setup para software SPI en configuracion de LED MATRIX en la ESP32CAM:
 #define CS_PIN 15
 #define DATA_PIN 13
 #define CLK_PIN 14
@@ -37,22 +40,21 @@
 // Crea una nueva instancia de la clase MD_Parola con conexion hardware SPI:
 MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 #define BUF_SIZE 50
-char mensaje[BUF_SIZE] = {"Bienvenido "};
+
+char mensaje[BUF_SIZE] = {"Bienvenido "}; //mensaje inicial que se despliega en la LED MATRIX
 char bienvenido[BUF_SIZE] = {"Bienvenido"};
 
 //Datos de Conexion WiFi
-//const char* ssid = "UTNG_Docencia";
-//const char* password = "UtNg2020@#$";
-const char* ssid = "INFINITUM306B_2.4";
-const char* password = "5bAtmtmNXt";
-const char* tema = "capstone/utng/ledmatrix";
+const char* ssid = "UTNG_Docencia";
+const char* password = "UtNg2020@#$";
+const char* tema = "capstone/utng/ledmatrix";  //tema para desplegar mensaje de la LED MATRIX
 long lastMsg = 0;
 
 int value = 0;
 
 //Datos del broker MQTT
-const char* mqtt_server = "192.168.1.70"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
-IPAddress server(192,168,1,70);
+const char* mqtt_server = "172.16.2.189"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+IPAddress server(172,16,2,189);
 
 // Objetos
 WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
@@ -63,12 +65,13 @@ long timeNow, timeLast; // Variables de control de tiempo no bloqueante
 int data = 0; // Contador
 int wait = 5000;  // Indica la espera cada 5 segundos para envío de mensajes MQTT
 
-void startCameraServer();
+void startCameraServer();  //funcion que inicia la camara
 
 void setup() {
   // Iniciar comunicación serial
   Serial.begin (115200);
-  
+
+  //Configuracion de la ESP32
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -90,9 +93,8 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
+
+  // si PSRAM IC present, 
   if(psramFound()){
     config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
@@ -108,10 +110,10 @@ void setup() {
     pinMode(14, INPUT_PULLUP);
   #endif
 
-  // camera init
+  // inicia camara
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
+    Serial.printf("Inicializacion fallo con codigo de error 0x%x", err);
     return;
   }
 
@@ -130,7 +132,7 @@ void setup() {
     s->set_hmirror(s, 1);
   #endif
 
-  setup_wifi();  
+  setup_wifi(); //funcion que configura el wifi
 
   // Conexión con el broker MQTT
   client.setServer(server, 1883); // Conectarse a la IP del broker en el puerto indicado
@@ -147,7 +149,7 @@ void setup() {
 
 void setup_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
+  // Se inicia conectando a la red WiFi
   Serial.println();
   Serial.print("Conectando a ");
   Serial.println(ssid);
@@ -160,7 +162,7 @@ void setup_wifi() {
     Serial.print(".");
   }
 
-  startCameraServer();
+  startCameraServer(); //Funciona inicia nuevamente la camara
   
   Serial.print("Camara Lista! Use 'http://");
   Serial.print(WiFi.localIP());
@@ -186,7 +188,7 @@ void loop() {
 // Esta función permite tomar acciones en caso de que se reciba un mensaje correspondiente a un tema al cual se hará una suscripción
 void callback(char* topic, byte* message, unsigned int length) {
 
-   Serial.print("Message arrived on topic: ");
+  Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
   String messageTemp;
@@ -197,28 +199,28 @@ void callback(char* topic, byte* message, unsigned int length) {
   }
   Serial.println();
 
-  // Feel free to add more if statements to control more GPIOs with MQTT
 
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
-  // Changes the output state according to the message
+  // Si una mensaje es recibido al tema capstone/utng/ledmatrix y si no esta vacio
+  // le concatena a Bienvenido el texto que le sigue. Por ejemplo, Bienvenido Luis Juarez
   if (String(topic) == "capstone/utng/ledmatrix") {
     if(messageTemp != ""){
       char newMensaje[BUF_SIZE] = {""};      
       static char *cp = newMensaje;
 
-     for (int i = 0; i < strlen(bienvenido); i++) {
+     for (int i = 0; i < strlen(bienvenido); i++) { //Itera sobre todos los caracteres
        *cp = bienvenido[i];
        cp++;
      }
-     *cp = ' ';
+     *cp = ' '; 
      cp++;
      *cp = ' ';
      cp++;
+     //Se recorre mediante apuntadores los caracteres de la cadena
       for (int i = 0; i < length; i++) {
         *cp = messageTemp[i];
         cp++;
       }
-      *cp = '\0';
+      *cp = '\0'; //Asigna salto de lìnea
       cp = newMensaje;
       strcpy(mensaje, newMensaje);
       
@@ -229,8 +231,6 @@ void callback(char* topic, byte* message, unsigned int length) {
 
       myDisplay.displayClear(); // Limpia el Display
       myDisplay.displayReset();
-      //myDisplay.displayScroll(ptrMsg, PA_CENTER, PA_SCROLL_LEFT, 100);
-      
     }
   }
         
