c1e91d378e6a057ff2fc01813da430a14023f690
tuxmex <36056832+tuxmex@users.noreply.github.com>
2022-08-05 01:34:58 -0500

    Update PushNotification.kt

--- SICUTNG_APP/java/mx/tictac/sicutng/notification/PushNotification.kt
+++ SICUTNG_APP/java/mx/tictac/sicutng/notification/PushNotification.kt
@@ -1,8 +1,23 @@
 package mx.tictac.sicutng.notification
 
+/*
+ * 
+ * por: El Profe Tacho
+ * Fecha: 10 de julio de 2022
+ * 
+ * Definición de los datos de la notificación push.
+ * Referencias:
+ * https://es.linkedin.com/learning/desarrollo-android-esencial
+ * https://www.youtube.com/watch?v=kZJhB7LlcOU
+ */
+
+//Bibliotecas
 import mx.tictac.sicutng.notification.NotificationData
 
+//Datos de la notificación Push
 data class PushNotification (
+    // Datos de la notificación
     val data: NotificationData,
+    // A quién será enviada la notificación
     val to: String
 )
