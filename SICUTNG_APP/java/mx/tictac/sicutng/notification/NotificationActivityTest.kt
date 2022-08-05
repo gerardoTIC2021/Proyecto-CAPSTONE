aaf92f9365ede762addba85e29b2900f4ba976ad
tuxmex <36056832+tuxmex@users.noreply.github.com>
2022-08-05 01:26:22 -0500

    Update NotificationActivityTest.kt

--- SICUTNG_APP/java/mx/tictac/sicutng/notification/NotificationActivityTest.kt
+++ SICUTNG_APP/java/mx/tictac/sicutng/notification/NotificationActivityTest.kt
@@ -1,5 +1,16 @@
 package mx.tictac.sicutng.notification
+/*
+ * 
+ * por: El Profe Tacho
+ * Fecha: 10 de julio de 2022
+ * 
+ *  Definición de datos estáticos para el uso de notificaciones en firebase, con metodos de Tokens y recepción de notificaciones.
+ * Referencias:
+ * https://es.linkedin.com/learning/desarrollo-android-esencial
+ * https://www.youtube.com/watch?v=kZJhB7LlcOU
+ */
 
+//Bibliotecas
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,25 +26,34 @@ import kotlinx.coroutines.launch
 import mx.tictac.sicutng.R
 import retrofit2.Retrofit
 
+//Constante del tópico de la notificación
 const val TOPIC = "topic-notification"
 
+//Clase Actividad de prueba de notificaciones 
 class NotificationActivityTest : AppCompatActivity() {
+   //Etiquea de la actividad
     val TAG = "NotificationActivityTest"
 
+    //Creación de la actividad 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Vista de la actividad 
         setContentView(R.layout.activity_notification_test)
-
+        
+        //Preferencias compartidad para la notificación
         FirebaseService.sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
 
+        //Escuchador de inicio exitoso del token
         FirebaseMessaging.getInstance().token.addOnSuccessListener {
             FirebaseService.token = it
             etToken.setText(it)
 
         }
-
+        
+        //Instancia de Mensajería Firebase que se suscribe al topico.
         FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
 
+        //Escuchador del botón enviar al dar click coloca los valores de las cajas de texto y si no son vacios envia la notificación push
         btnSend.setOnClickListener {
             val title = etTitle.text.toString()
             val message = etMessage.text.toString()
@@ -49,12 +69,16 @@ class NotificationActivityTest : AppCompatActivity() {
         }
     }
 
+    //Método que envia la notificación push mandando mensajes al LogCat en casos de respuesta exitosa, con errores y con excepciones.
     private fun senNotification(notification: PushNotification) = CoroutineScope(Dispatchers.IO).launch {
         try {
+            //Si se envia la notificación inicia la respuesta del servicio--
             val response = RetrofitInstance.api.postNotification(notification)
             if(response.isSuccessful){
+                //Valor enviado al registro con la respuesta en formato JSON
                 Log.d(TAG, "Response: ${Gson().toJson(response)}")
             }else{
+                //Mensaje con el cuerpo de los errores de la respuesta 
                 Log.e(TAG, response.errorBody().toString())
             }
         }catch (e: Exception){
