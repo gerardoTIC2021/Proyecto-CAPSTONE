4a8422b06450f352153d6aefe3715feed4d3d8c4
tuxmex <36056832+tuxmex@users.noreply.github.com>
2022-08-05 01:31:13 -0500

    Update NotificationData.kt

--- SICUTNG_APP/java/mx/tictac/sicutng/notification/NotificationData.kt
+++ SICUTNG_APP/java/mx/tictac/sicutng/notification/NotificationData.kt
@@ -1,6 +1,18 @@
 package mx.tictac.sicutng.notification
+/*
+ * por: El Profe Tacho
+ * Fecha: 10 de julio de 2022
+ * 
+ * Clase de datos de la notificación.
+ * Referencias:
+ * https://es.linkedin.com/learning/desarrollo-android-esencial
+ * https://www.youtube.com/watch?v=kZJhB7LlcOU
+ */
 
+//Una data class no es más que una clase que sólo contiene estado y no realiza ninguna operación. 
 data class NotificationData (
+    //Título de la notificación
     val title: String,
+    //Mensaje de la notificación
     val message: String
 )
