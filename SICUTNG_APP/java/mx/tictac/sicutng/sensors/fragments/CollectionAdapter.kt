8056ec7ce208b3f6c7f4c0ed2df56ab3e7d63d90
tuxmex <36056832+tuxmex@users.noreply.github.com>
2022-08-05 03:00:00 -0500

    Update CollectionAdapter.kt

--- SICUTNG_APP/java/mx/tictac/sicutng/sensors/fragments/CollectionAdapter.kt
+++ SICUTNG_APP/java/mx/tictac/sicutng/sensors/fragments/CollectionAdapter.kt
@@ -1,20 +1,38 @@
 package mx.tictac.sicutng.sensors.fragments
 
+/*
+ *
+ * por: El Profe Tacho
+ * Fecha: 10 de julio de 2022
+ * Adaptador de colección para el View Pager.
+ * 
+ * Referencias:
+ * https://www.youtube.com/watch?v=ShsoW5lFflw
+ *
+ */
+
+
+//Bibliotecas
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class CollectionAdapter(fm: FragmentActivity) :
-    FragmentStateAdapter(fm) {
 
+//Clase para adaptar la colección de fragementos de pantalla al View Pager.
+class CollectionAdapter(fm: FragmentActivity) : FragmentStateAdapter(fm) {
+    //Numero de paginas (fragmentos del view pager)
     override fun getItemCount(): Int {
-        return 2
+        return 2 //Dos fragmentos 
     }
-
+    
+    //Método para la creación de fragmentos para el fragmento de página para temperatura y humedad.
     override fun createFragment(position: Int): Fragment {
         return when (position) {
+            //Posición 0 para el fragmento de temperatura
             0 -> TemperatureFragment()
+            //Posición 1 para el fragmento de Humedad
             1 -> HumidityFragment()
+            // No hay al momento más fragmentos de los arriba descritos.
             else -> throw IllegalStateException("Unexpected position $position")
         }
     }
